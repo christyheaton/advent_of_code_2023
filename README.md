221b16feb521b93fedb65247e72fa69618db2980
christyheaton <christyheaton@gmail.com>
2023-12-05 21:54:22 -0800

    environment and prelim day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+Advent of code 2023
+
+Using Python 3.12 and poetry
---
+++ src/day01.py
@@ -0,0 +1,13 @@
+
+def find_first_last_digit(line):
+    pass
+
+
+def main():
+    with open("../data/day01_test.txt") as file:
+        lines = [line.rstrip() for line in file]
+    print(lines)
+
+
+if __name__ == "__main__":
+    main()
