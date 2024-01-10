53424e5a9b6f6c0b56687f64380462426960f48e
christyheaton <christyheaton@gmail.com>
2024-01-09 19:41:51 -0800

    day 4 partial part 2

--- src/day04.py
+++ src/day04.py
@@ -1,7 +1,12 @@
 class Card:
     def __init__(self, line: str):
         self.line = line
+        self.id = int(self.line.split(":")[0].split()[1])
         self.data = self.parse_card()
+        self.score = 0
+
+    def __repr__(self):
+        return f"{self.id}: {self.score}"
 
     def parse_card(self) -> dict:
         winning_numbers = self.line.split(":")[1].split("|")[0].split()
@@ -16,21 +21,55 @@ class Card:
     def calculate_score(self) -> int:
         wins = self.count_wins()
         if wins > 2:
-            score = 2 ** (wins - 1)
+            self.score = 2 ** (wins - 1)
         else:
-            score = wins
-        return score
+            self.score = wins
+        return self.score
+
+
+class CardCounter:
+    def __init__(self, cards):
+        self.cards = cards
+        self.card_counts = {}
+        self.get_initial_card_counts()
+
+    def get_initial_card_counts(self):
+        for card in self.cards:
+            self.card_counts[int(card.id)] = 1
+
+    def count_cards(self):
+        for card in self.cards:
+            # print(f"Card {card.id} has {self.card_counts.get(card.id)} copies and score of {card.score}.")
+            score = card.score
+            for i in range(score):
+                current_index = card.id + (i+1)
+
+                if current_index > len(self.cards):
+                    continue
+                current_count = self.card_counts.get(current_index)
+                # print(f"Trying to modify card {current_index} with count of {current_count}")
+                self.card_counts[current_index] = current_count + 1
+                # print(f"Count is now: ", self.card_counts.get(current_index))
+        print(self.card_counts)
+            # return self.card_counts
 
 
 def main() -> None:
-    with open("../data/day04.txt") as file:
+    with open("../data/day04_test.txt") as file:
         lines = [line.rstrip() for line in file]
 
+    cards = []
     total_score = 0
     for line in lines:
         c = Card(line)
+        cards.append(c)
+        # print(c.id)
         total_score += c.calculate_score()
     print(f"Part 1: {total_score}")
+    # print(cards)
+
+    counter = CardCounter(cards)
+    print(f"Part 2: {counter.count_cards()}")
 
 
 if __name__ == "__main__":
